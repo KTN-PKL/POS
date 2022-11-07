f56ad8e7fc3c9916ccd7faf38383a5ab5e6b2460
gusnanda7 <gus11111234@gmail.com>
2022-11-07 09:11:32 +0700

    comit

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -1,5 +1,17 @@
 <div class="p2">
     <div class="form-group">
+        <input type="text" name="item" id="kategori" class="form-control" placeholder="Masukan Kategori">
+    </div>
+    <div class="form-group">
+        <input type="text" name="kategori" id="kategori" class="form-control" placeholder="Masukan Kategori">
+    </div>
+    <div class="form-group">
+        <input type="text" name="kategori" id="kategori" class="form-control" placeholder="Masukan Kategori">
+    </div>
+    <div class="form-group">
+        <input type="text" name="kategori" id="kategori" class="form-control" placeholder="Masukan Kategori">
+    </div>
+    <div class="form-group">
         <input type="text" name="kategori" id="kategori" class="form-control" placeholder="Masukan Kategori">
     </div>
     <div class="form-group mt-2">
