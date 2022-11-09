5660fbc9ed9ca8d932d98f515590eb52ee5cadbd
bagaas86 <bagaas86@gmail.com>
2022-11-09 14:54:50 +0700

    commiittsss

--- resources/views/v_dashboard.blade.php
+++ resources/views/v_dashboard.blade.php
@@ -1,3 +1,8 @@
+<style type="text/css">
+    .lihat{
+        color: #0c4e68
+    }
+</style>
 @extends('layout.template')
 @section('content')
 <div class="jumbotron p-5 rounded-3" style="background-color: rgb(240, 240, 240)">
@@ -5,25 +10,29 @@
     <div class="row">
      <div style="background-color: rgb(240, 240, 240)  ;box-shadow:none; border:none;" class="card col-sm-3" >
         <div class="card card-rounded mb-4 ">
-        <div style="" class="card-header bg-primary text-white">
+        <div style="background-color:#0c4e68" class="card-header text-white">
             <h4>Kategori</h4>
         </div>
             <div class="card-body">
                 <h3 style="font-size:36px; text-align:center;" data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></h3>
-                <a style="text-decoration: none" href="#"><h5><b>Lihat Selengkapnya</b>
+            </div> 
+            <div style="background-color: white" class="card-footer">
+                <a style="text-decoration: none" href="#"><h5 class="lihat" >Lihat Selengkapnya
                     <i class="fa fa-arrow-right"></i></h5></a>
-            </div>    
+            </div>       
     </div>
     </div>
 
     <div style="background-color:rgb(240, 240, 240)  ;box-shadow:none; border:none;" class="card col-sm-3" >
         <div class="card card-rounded mb-4 ">
-        <div class="card-header bg-primary text-white">
+        <div style="background-color:#0c4e68" class="card-header  text-white">
             <h4>Menu</h4>
         </div>
             <div class="card-body">
                 <h3 style="font-size:36px; text-align:center;" data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></h3>
-                <a style="text-decoration: none" href="#"><h5><b>Lihat Selengkapnya</b>
+            </div>    
+            <div style="background-color: white" class="card-footer">
+                <a style="text-decoration: none" href="#"><h5 class="lihat" >Lihat Selengkapnya
                     <i class="fa fa-arrow-right"></i></h5></a>
             </div>    
     </div>
@@ -31,12 +40,14 @@
 
     <div style="background-color: rgb(240, 240, 240) ;box-shadow:none; border:none;" class="card col-sm-3" >
         <div class="card card-rounded mb-4 ">
-        <div class="card-header bg-primary text-white">
+        <div style="background-color: #0c4e68" class="card-header text-white">
             <h4>Customer</h4>
         </div>
             <div class="card-body">
                 <h3 style="font-size:36px; text-align:center;" data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></h3>
-                <a style="text-decoration: none" href="#"><h5><b>Lihat Selengkapnya</b>
+            </div>    
+             <div style="background-color: white" class="card-footer">
+                <a style="text-decoration: none" href="#"><h5 class="lihat" >Lihat Selengkapnya
                     <i class="fa fa-arrow-right"></i></h5></a>
             </div>    
     </div>
@@ -44,24 +55,28 @@
 
     <div style="background-color:rgb(240, 240, 240)  ;box-shadow:none; border:none;" class="card col-sm-3" >
         <div class="card card-rounded mb-4 ">
-        <div class="card-header bg-primary text-white">
+        <div style="background-color: #0c4e68" class="card-header  text-white">
             <h4>Transaksi Selesai</h4>
         </div>
             <div class="card-body">
                 <h3 style="font-size:36px; text-align:center;" data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></h3>
-                <a style="text-decoration: none" href="#"><h5><b>Lihat Selengkapnya</b>
+            </div>   
+            <div style="background-color: white" class="card-footer">
+                <a style="text-decoration: none" href="#"><h5 class="lihat" >Lihat Selengkapnya
                     <i class="fa fa-arrow-right"></i></h5></a>
-            </div>    
+            </div>     
     </div>
     </div>
     <div style="background-color: rgb(240, 240, 240)  ;box-shadow:none; border:none;" class="card col-sm-12" >
         <div class="card card-rounded mb-4 ">
-        <div class="card-header bg-primary text-white">
+        <div style="background-color: #0c4e68" class="card-header text-white">
             <h4>Laporan Penjualan 2022</h4>
         </div>
             <div class="card-body">
                 <h3 style="font-size:36px; text-align:center;" data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></h3>
-                <a style="text-decoration: none" href="#"><h5><b>Lihat Selengkapnya</b>
+            </div>
+            <div style="background-color: white" class="card-footer">
+                <a style="text-decoration: none" href="#"><h5 class="lihat" >Lihat Selengkapnya
                     <i class="fa fa-arrow-right"></i></h5></a>
             </div>    
     </div>
