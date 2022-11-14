dfb3cf33c6b6af1751c1045976bb956214585756
gusnanda7 <gus11111234@gmail.com>
2022-11-14 12:51:06 +0700

    comit

--- resources/views/stok/create.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="p2">
-    <div class="form-group">
-        <input type="text" name="kategori" id="kategori" class="form-control" placeholder="Masukan Kategori">
-    </div>
-    <div class="form-group mt-2">
-        <button class="btn btn-success" onClick="store()">Create</button>
-    </div>
-</div>
