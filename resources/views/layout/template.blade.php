9d88e074b2f03ff278d9a99b3b2fa9356888f281
bagaas86 <bagaas86@gmail.com>
2022-11-16 15:34:16 +0700

    vieww

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -162,11 +162,11 @@
 <script src="{{ asset('template2') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('template2') }}/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('template2') }}/dist/js/demo.js"></script>
+{{-- <!-- AdminLTE for demo purposes -->
+<script src="{{ asset('template2') }}/dist/js/demo.js"></script> --}}
 <script src="{{ asset('template2') }}/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('template2') }}/dist/js/demo.js"></script>
+{{-- <!-- AdminLTE for demo purposes -->
+<script src="{{ asset('template2') }}/dist/js/demo.js"></script> --}}
 
 <script>src="https://code.jquery.com/jquery-3.5.1.js"</script>
 <script>src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"</script>
