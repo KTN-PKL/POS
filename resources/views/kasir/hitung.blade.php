7b6d94c4024d638d10f04cf1259d99542b64f68f
gusnanda7 <gus11111234@gmail.com>
2022-11-16 15:24:47 +0700

    comit

--- resources/views/kasir/hitung.blade.php
+++ resources/views/kasir/hitung.blade.php
@@ -24,24 +24,7 @@
             </select>
         </td> 
     </tr>
-    <tr> 
-        <td>
-        <label for="FirstName">TOTAL BAYAR</label>
-        <small class="text-danger"></small>
-        </td> 
-        <td>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">Rp</span>
-                </div>
-                @php
-                    $isi = number_format($total,2,",",".");
-                @endphp
-                <input type="text" class="form-control" placeholder="Nama Customer" aria-label="Recipient's username" aria-describedby="basic-addon2" value="{{ $isi }}" readonly>
-               
-              </div>
-        </td> 
-    </tr>
+    <tr id="total"></tr>
     <tr> 
         <td>
         <label for="FirstName">DISKON</label>
