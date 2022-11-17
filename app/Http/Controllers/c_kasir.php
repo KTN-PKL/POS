6f56eb393a63a739fa0c2be4c11c095b90f5597c
gusnanda7 <gus11111234@gmail.com>
2022-11-17 12:20:11 +0700

    comit

--- app/Http/Controllers/c_kasir.php
+++ app/Http/Controllers/c_kasir.php
@@ -167,13 +167,13 @@ class c_kasir extends Controller
         $t = (int) str_replace(".","",$request->total);
         $dr = ($request->discountrate * $t)/100;
         $pr = ($request->pajakrate * $t)/100;
-        $d = $request->discount;
-        $p = $request->pajak;
+        $d = (int) str_replace(".","",$request->discount);
+        $p = (int) str_replace(".","",$request->pajak);
         $gt = $t - $dr + $pr - $d + $p;
         $data = [
             'grandtotal' => $gt,
         ];
         return view('kasir.grandtotal', $data);
     }
-    
+
 }
