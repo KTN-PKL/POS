528259b7b5e99e5dc734052b683ba289f742f2de
gusnanda7 <gus11111234@gmail.com>
2022-11-30 10:18:16 +0700

    comit

--- resources/views/order/edit.blade.php
+++ resources/views/order/edit.blade.php
@@ -1,18 +1,14 @@
 <div class="p2">
     <div class="form-group">
-        <label class="form-label">Akun</label>
-        <input type="text" name="akun" id="akun" class="form-control" placeholder="Masukan Akun" value="{{ $akuntansi->akun }}">
-        <small style="display: none" class="text-danger" id="required1">Akun Harus Diisi!</small>
-        <small style="display: none" class="text-danger" id="unique">Akun Sudah Ada!</small>
-        <br>
-        <label class="form-label">Jenis</label>
-        <select class="form-select" id="jenis">
-            <option @if ($akuntansi->jenis == "Pemasukan") selected @endif value="Pemasukan">Pemasukan</option>\
-            <option @if ($akuntansi->jenis == "Pengeluaran") selected @endif value="Pengeluaran">Pengeluaran</option>
-        </select>
+        <label class="form-label">Grandtotal</label>
+        <input type="text" id="grantotal" class="form-control" placeholder="Masukan Akun" value="{{ $transaksi->grantotal }}" readonly>
+        <label class="form-label">Bayar</label>
+        <input type="text" id="bayar" class="form-control" placeholder="Masukan Akun" value="0">
+        <label class="form-label">Kembali</label>
+        <input type="text" id="bayar" class="form-control" placeholder="Masukan Akun" value="{{ $akuntansi->akun }}">
     </div>
     <div class="form-group mt-2">
-        <button class="btn btn-success" onClick="update({{ $akuntansi->id_akun }})">Edit</button>
+        <button class="btn btn-success" onClick="update({{ $akuntansi->id_akun }})">Bayar</button>
     </div>
 </div>
 
