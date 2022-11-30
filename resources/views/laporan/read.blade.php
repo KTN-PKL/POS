f56095b5eb253926d76157ae44e3016c59748d4f
bagaas86 <bagaas86@gmail.com>
2022-11-30 08:41:24 +0700

    commit

---
+++ resources/views/laporan/read.blade.php
@@ -0,0 +1,36 @@
+<div class="card">
+    <div style="background-color:#0c4e68" class="card-header">
+        <h5 class="text-white" > <i class="fa fa-tag"></i> <b>Laporan Transaksi</b></h5>
+    </div>
+    <div class="card-body">
+        <div class="input-group col-md-4 offset-8">
+            <input onkeyup="cari()" id="cari" type="search" class="form-control input-sm" placeholder="Search Kategori" aria-label="Search" aria-describedby="search-addon" />     
+            {{-- <button type="button" class="btn btn-outline-primary">Search</button> --}}
+          </div>
+          <br>
+      
+<div id="table"></div>
+</div>
+</div>
+
+
+<script>
+     $(document).ready(function() {
+        table()
+    });
+    function table() {
+            $.get("{{ url('kategori/table') }}", {}, function(data, status) {
+                $("#table").html(data);
+            });
+        }
+    function cari() {
+            var cari = $("#cari").val();
+            if (cari == "") {
+                table()
+            } else {
+                $.get("{{ url('kategori/cari') }}/" + cari, {}, function(data, status) {
+                $("#table").html(data);
+            });
+            }
+        }
+</script>
