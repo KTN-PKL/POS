5a0d5eedcfb2607c06ff35fc249bc50f4eb7c550
gusnanda7 <gus11111234@gmail.com>
2022-12-01 08:11:50 +0700

    comit

--- resources/views/order/edit.blade.php
+++ resources/views/order/edit.blade.php
@@ -1,14 +1,20 @@
 <div class="p2">
     <div class="form-group">
         <label class="form-label">Grandtotal</label>
-        <input type="text" id="grantotal" class="form-control" placeholder="Masukan Akun" value="{{ $transaksi->grantotal }}" readonly>
+        @php
+            $grandtotal = number_format($transaksi->grandtotal,0,",",".");
+            $kode = $transaksi->id_transaksi;
+                $urutan = (int) substr($kode, 3, 3);
+        @endphp
+        <input type="text" id="grandtotal" class="form-control" placeholder="Masukan Akun" value="{{ $grandtotal }}" readonly>
         <label class="form-label">Bayar</label>
-        <input type="text" id="bayar" class="form-control" placeholder="Masukan Akun" value="0">
+        <input type="text" class="form-control" id="bayar" value="0" aria-describedby="basic-addon2" onkeyup="bayar()">
         <label class="form-label">Kembali</label>
-        <input type="text" id="bayar" class="form-control" placeholder="Masukan Akun" value="{{ $akuntansi->akun }}">
+        <input type="text" id="kembalian" class="form-control" value="0" readonly>
+        <small style="display: none" class="text-danger" id="kurang">Pembayaran Kurang!</small>
     </div>
     <div class="form-group mt-2">
-        <button class="btn btn-success" onClick="update({{ $akuntansi->id_akun }})">Bayar</button>
+        <button class="btn btn-success" onClick="updated({{ $urutan }})">Bayar</button>
     </div>
 </div>
 
