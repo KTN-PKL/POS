e6fac776b602e1f5e40c4f11b48c3f5f340577aa
bagaas86 <bagaas86@gmail.com>
2022-12-01 14:13:40 +0700

    committ

--- app/Exports/transaksiExport.php
+++ app/Exports/transaksiExport.php
@@ -3,19 +3,31 @@
 namespace App\Exports;
 
 use App\Models\transaksi;
+use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class transaksiExport implements FromCollection, WithHeadings
+class transaksiExport implements FromView
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    // public function collection()
+    // {
+    //     return transaksi::all()->where('dikirim');
+    // }
+    public function __construct()
     {
-        return transaksi::all()->where('dikirim');
+        $this->transaksi = new transaksi();
     }
 
+    public function view(): View
+    {
+        return view('laporan.table', [
+            'transaksi' => $this->transaksi->allData(),
+        ]);
+    }
     // public function headings():array
     // {
     //     return[]
