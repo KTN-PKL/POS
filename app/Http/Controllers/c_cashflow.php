1e3f45b119f110a9957d453c3e0894b0920fc201
gusnanda7 <gus11111234@gmail.com>
2022-12-03 10:57:38 +0700

    comit

---
+++ app/Http/Controllers/c_cashflow.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\transaksi;
+use App\Models\keranjang;
+
+class c_cashflow extends Controller
+{
+    public function __construct()
+    {
+        $this->keranjang = new keranjang();
+        $this->transaksi = new transaksi();
+    }
+
+    public function index1()
+    {
+        $data = [
+            'view' => "all",
+        ];
+        return view('cashflow.index', $data);
+    }
+
+
+}
