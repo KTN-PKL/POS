8388cde7e408aaecfa2d1aafa5438fbcc3051497
gusnanda7 <gus11111234@gmail.com>
2022-12-08 11:44:37 +0700

    comit

---
+++ resources/views/ates.blade.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cara Menggunakan Datatables | Malas Ngoding</title>	
+	<!-- Bootstrap CSS -->
+<!-- Bootstrap DataTables CSS -->
+<!-- Jquery -->
+
+<script type="text/javascript" language="javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<!-- Jquery DataTables -->
+<script type="text/javascript" language="javascript" src="http:////cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
+<!-- Bootstrap dataTables Javascript -->
+<script type="text/javascript" language="javascript" src="http://cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+</head>
+<body>
+	<br/>
+	<br/>
+	<div class="container">
+		<table class="table table-striped table-bordered data">
+			<thead>
+				<tr>			
+					<th>Nama</th>
+					<th>Alamat</th>
+					<th>Pekerjaan</th>
+					<th>Usia</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Malas Ngoding</td>
+					<td>Bandung</td>
+					<td>Web Developer</td>
+					<td>26</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Malas Ngoding</td>
+					<td>Bandung</td>
+					<td>Web Developer</td>
+					<td>26</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+				<tr>				
+					<td>Andi</td>
+					<td>Jakarta</td>
+					<td>Web Designer</td>
+					<td>21</td>
+					<td>Aktif</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</body>
+
+</html>
