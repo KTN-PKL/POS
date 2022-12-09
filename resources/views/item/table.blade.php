64381146cf8a22d7f18fa9ba76ebd7cc9bb76b10
bagaas86 <bagaas86@gmail.com>
2022-12-09 10:14:26 +0700

    committ

--- resources/views/item/table.blade.php
+++ resources/views/item/table.blade.php
@@ -1,15 +1,35 @@
-<table class="table table-bordered table-hover" style="width: 100%">
-    <tr>
-        <th>No</th>
-        <th>Kode Item</th>
-        <th>Kategori</th>
-        <th>Nama Item</th>
-        <th>Harga Beli</th>
-        <th>Harga Jual</th>
-        <th>Gambar</th>
-        <th>Action</th>
-    </tr>
-    @php
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD Laravel 8</title>
+
+    <!-- Jquery DataTables -->
+<script type="text/javascript" language="javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" language="javascript" src="http:////cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" language="javascript" href="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+
+</head>
+
+<table id="item" class="table table-bordered table-hover" style="width: 100%">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Kode Item</th>
+            <th>Kategori</th>
+            <th>Nama Item</th>
+            <th>Harga Beli</th>
+            <th>Harga Jual</th>
+            <th>Gambar</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+   
+    <tbody>
+        @php
         $i = 0;
     @endphp
     @foreach ($item as $items)
@@ -34,4 +54,13 @@
             </td>
         </tr>
     @endforeach
+    </tbody>
+   
 </table>
+
+<script type="text/javascript" charset="utf-8">
+    $(document).ready(function() {
+        $('#item').DataTable();
+            document.getElementById("item_filter").style.display = "none";
+    });
+</script>
--- resources/views/laporan/table.blade.php
+++ resources/views/laporan/table.blade.php
@@ -3,40 +3,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CRUD Laravel 8</title>
-        <script type="text/javascript" language="javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-<!-- Jquery DataTables -->
+
+    <!-- Jquery DataTables -->
+<script type="text/javascript" language="javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script type="text/javascript" language="javascript" src="http:////cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
-{{-- <!-- Bootstrap dataTables Javascript -->
-<script type="text/javascript" language="javascript" src="http://cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
-<script type="text/javascript" language="javascript" src="http://cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script> --}}
-
-<script type="text/javascript" href="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script type="text/javascript" href="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" href="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
-
+<script type="text/javascript" language="javascript" href="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-<style>
-    .dataTables_wrapper .dataTables_paginate .paginate_button:hover {
-  background: none;
-  color: black!important;
-  border-radius: 4px;
-  border: 1px solid #828282;
-}
- 
-.dataTables_wrapper .dataTables_paginate .paginate_button:active {
-  background: none;
-  color: black!important;
-}
-</style>
-
-
-
 
 </head>
 <body>
-
 <table id="data" class="table table-striped table-bordered" width="100%" cellspacing="0">
   <thead>
     <tr>
